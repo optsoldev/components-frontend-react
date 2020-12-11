d7405b77cec14c2ff3ccdeb730a09281dacc5b8c
Felipe Carvalho <contato@felipecarvalho.net>
2020-12-11 00:31:33 -0300

    Ajustando e adicionando configurações

--- sdk-front-end/src/shared/styles/global.ts
+++ sdk-front-end/src/shared/styles/global.ts
@@ -2,21 +2,16 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
   * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
   }
-
-  html {
-    font-size: 62.5% // Para utilizar REM 1rem === 10px
-  }
-
   body {
-    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif // Reseta para fonts padrões dos dispositivos
+      font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif // Reseta para fonts padrões dos dispositivos
   }
 
   html, body, #root {
-    height: 100%;
+      height: 100%;
   }
 `;
 
