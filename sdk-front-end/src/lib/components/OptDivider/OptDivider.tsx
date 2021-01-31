9bc291f5104b7fe730e25113af5ee782b5afe1f7
felipedsc <felipe_fsc@hotmail.com>
2021-01-31 00:13:36 -0300

    Permitindo informar margin do optdivider

--- sdk-front-end/src/lib/components/OptDivider/OptDivider.tsx
+++ sdk-front-end/src/lib/components/OptDivider/OptDivider.tsx
@@ -1,6 +1,11 @@
 import { Divider } from '@material-ui/core';
 import styled from 'styled-components';
 
-export const OptDivider = styled(Divider)`
+interface OptDividerProps {
+  marginY?: number;
+}
+
+export const OptDivider = styled(Divider)<OptDividerProps>`
   background-color: ${({ theme }) => theme.drawer.divider};
+  ${({ marginY }) => `margin: ${marginY}px 0;`}
 `;
