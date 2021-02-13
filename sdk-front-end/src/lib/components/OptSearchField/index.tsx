350ec8f979841a08b4ea128e546c1ff0a5b1faac
felipedsc <felipe_fsc@hotmail.com>
2021-02-12 23:37:54 -0300

    Adicionando componente de pesquisa + ajustes

---
+++ sdk-front-end/src/lib/components/OptSearchField/index.tsx
@@ -0,0 +1,34 @@
+import { ButtonBase } from '@material-ui/core';
+import { mdiMagnify } from '@mdi/js';
+import Icon from '@mdi/react';
+import React, { createRef } from 'react';
+import { ColorPalette } from '../../shared/styles/colors';
+import * as S from './styles';
+
+interface Props {
+  placeholder?: string;
+  onSearch: (searchTerm?: string) => void;
+}
+
+export const OptSearchField = ({ placeholder = 'Pesquisar', onSearch }: Props) => {
+  const ref = createRef<HTMLInputElement>();
+
+  function onClickSearchButton() {
+    onSearch(ref.current?.value ? ref.current?.value : undefined);
+  }
+
+  function verificarTeclaPressionadaEnter(event: React.KeyboardEvent<HTMLDivElement>) {
+    if (event.key === 'Enter') {
+      onSearch(ref.current?.value ? ref.current?.value : undefined);
+    }
+  }
+
+  return (
+    <S.AdvancedSearchContainer>
+      <input type="text" placeholder={placeholder} ref={ref} onKeyDown={verificarTeclaPressionadaEnter} />
+      <ButtonBase onClick={onClickSearchButton}>
+        <Icon size={0.8} path={mdiMagnify} color={ColorPalette.white} />
+      </ButtonBase>
+    </S.AdvancedSearchContainer>
+  );
+};
