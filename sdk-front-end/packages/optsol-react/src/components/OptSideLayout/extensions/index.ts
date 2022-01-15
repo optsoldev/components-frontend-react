da670d11fcdaf931d03d5d71640bb0c8fc70e6ad
Felipe Carvalho <felipe.carvalho@optsol.com.br>
2022-01-15 15:31:13 -0300

    Exportando  OptSidebarListBaseContainer

--- sdk-front-end/packages/optsol-react/src/components/OptSideLayout/extensions/index.ts
+++ sdk-front-end/packages/optsol-react/src/components/OptSideLayout/extensions/index.ts
@@ -2,4 +2,5 @@ export { OptSidebarFilterContainer } from './OptSidebarFilterContainer';
 export { OptFilterLink } from './OptFilterLink';
 export { OptSidebarListContainer } from './OptSidebarListContainer';
 export { OptSidebarPaginatedListContainer } from './OptSidebarPaginatedListContainer';
+export { OptSidebarListBaseContainer } from './OptSidebarListBaseContainer';
 export * from '.';
