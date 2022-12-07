8d21dc7afe458f06d94368f2a3aad970d0d9a996
lucas.alonso <lucas.alonso@optsol.com.br>
2022-12-06 22:56:55 -0300

    Ajustes webpack storyBook

--- examples/storybook/.storybook/main.js
+++ examples/storybook/.storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
+  core: {
+    builder: 'webpack5',
+  },
   "stories": [
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)"
@@ -6,5 +9,5 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
 }
