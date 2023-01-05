cb7fe6af866eccbbc7b6e291d31d9f14db6c278b
lucas.alonso <lucas.alonso@optsol.com.br>
2023-01-05 11:54:38 -0300

    Correção em célula do ActionButton OptGrid

--- packages/optsol-react/src/components/OptGrid/OptGridActionsCell.tsx
+++ packages/optsol-react/src/components/OptGrid/OptGridActionsCell.tsx
@@ -17,41 +17,43 @@ export function OptGridActionsCell<T extends object>({
   const { currentTheme } = useOptTheme();
 
   return (
-    <td className="td-opt" style={{ display: 'flex' }}>
-      {actions?.map((_action, index) => {
-        const isFunction = typeof _action === 'function';
+    <td className="td-opt">
+      <div style={{ display: 'flex' }}>
+        {actions?.map((_action, index) => {
+          const isFunction = typeof _action === 'function';
 
-        let action: OptGridAction<T>;
+          let action: OptGridAction<T>;
 
-        if (isFunction) action = _action(data);
-        else action = _action;
+          if (isFunction) action = _action(data);
+          else action = _action;
 
-        let color = action.icon.color ?? currentTheme.color;
-        color = action.disabled ? ColorPalette.gray4 : color;
+          let color = action.icon.color ?? currentTheme.color;
+          color = action.disabled ? ColorPalette.gray4 : color;
 
-        const iconButton = (
-          <IconButton
-            key={index}
-            onClick={(e) => {
-              e.stopPropagation();
-              action.onClick(e, data);
-            }}
-            disabled={action.disabled}
-          >
-            <Icon size={1} path={action.icon.path} color={color} />
-          </IconButton>
-        );
-
-        if (action.tooltip) {
-          return (
-            <Tooltip title={action.tooltip} key={index}>
-              {iconButton}
-            </Tooltip>
+          const iconButton = (
+            <IconButton
+              key={index}
+              onClick={(e) => {
+                e.stopPropagation();
+                action.onClick(e, data);
+              }}
+              disabled={action.disabled}
+            >
+              <Icon size={1} path={action.icon.path} color={color} />
+            </IconButton>
           );
-        }
 
-        return iconButton;
-      })}
+          if (action.tooltip) {
+            return (
+              <Tooltip title={action.tooltip} key={index}>
+                {iconButton}
+              </Tooltip>
+            );
+          }
+
+          return iconButton;
+        })}
+      </div>
     </td>
   );
 }
