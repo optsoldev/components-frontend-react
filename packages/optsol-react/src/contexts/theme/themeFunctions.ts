5ee60397789cf8cd90df37cb5d7915958298304d
Vladimir Christ <vladimir.christ@optsol.com.br>
2023-08-15 17:52:03 -0300

    Update merge theme error

--- packages/optsol-react/src/contexts/theme/themeFunctions.ts
+++ packages/optsol-react/src/contexts/theme/themeFunctions.ts
@@ -1,30 +1,14 @@
+import merge from 'lodash.merge';
+
 import { OptFullTheme } from '../../shared/styles/theme';
-import { RecursivePartial } from '../../types/RecursivePartial';
-
-export function copyInto<T>(to: T, from: RecursivePartial<T>) {
-  if (typeof from === 'object') {
-    const target = to;
-    Object.entries(from).forEach(([key, value]) => {
-      Object.defineProperty(target, key, {
-        value: copyInto(to[key], value),
-      });
-    });
-
-    return target;
-  }
-
-  return from;
-}
 
 export function generateNewTheme(
   theme: OptFullTheme,
-  customTheme?: RecursivePartial<OptFullTheme>
+  customTheme?: Partial<OptFullTheme>
 ) {
-  let newTheme = JSON.parse(JSON.stringify(theme)) as OptFullTheme;
+  const newTheme = JSON.parse(JSON.stringify(theme)) as OptFullTheme;
 
-  if (customTheme) {
-    newTheme = copyInto(newTheme, customTheme);
-  }
+  if (customTheme) merge(newTheme, customTheme);
 
   return newTheme;
 }
